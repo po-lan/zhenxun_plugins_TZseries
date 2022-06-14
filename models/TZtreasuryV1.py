5ca549033937f67b87a2f4d21123f560429d1cd3
po-lan <42771836+po-lan@users.noreply.github.com>
2022-06-14 17:58:31 +0800

    Update TZtreasuryV1.py

--- models/TZtreasuryV1.py
+++ models/TZtreasuryV1.py
@@ -7,9 +7,6 @@ class TZtreasury(db.Model):
     group_id = db.Column(db.BigInteger(), nullable=False)
     money = db.Column(db.BigInteger(), nullable=False, default=0)
 
-    welfare_lottery  = db.Column(db.BigInteger(), nullable=False, default=1000)
-
-
     _idx1 = db.Index("tz_treasury_idx1", "group_id", unique=True)
 
     @classmethod
@@ -56,27 +53,3 @@ class TZtreasury(db.Model):
         else:
             await cls.create( group_id=group_id, money=10000)
             return 10000
-
-    
-    @classmethod
-    async def getLotteryGold(cls, group_id: int):
-        query = cls.query.where(cls.group_id == group_id)
-        query = query.with_for_update()
-        my = await query.gino.first()
-
-        if my:
-            return my.welfare_lottery
-        else:
-            await cls.create( group_id=group_id, welfare_lottery=1000)
-            return 1000
-
-    @classmethod
-    async def setLotteryGold(cls,  group_id: int, num: int):
-        query = cls.query.where(cls.group_id == group_id)
-        query = query.with_for_update()
-        my = await query.gino.first()
-
-        if my:
-            await  my.update(welfare_lottery = num).apply()
-        else:
-            await cls.create( group_id=group_id, welfare_lottery = 1000 + num)
