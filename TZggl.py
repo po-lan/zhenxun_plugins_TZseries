b82ba266143ba0062d8fbfb18e89ce4ee6d259e5
yajiwa <839790708@qq.com>
2022-06-21 15:13:36 +0800

    update Tzggl

--- TZggl.py
+++ TZggl.py
@@ -1,4 +1,7 @@
 import re
+
+from nonebot.rule import to_me
+
 from services.db_context import db
 from nonebot import on_command
 from nonebot.params import CommandArg
@@ -12,13 +15,13 @@ from configs.config import NICKNAME, Config
 import random
 from .models.TZtreasuryV1 import TZtreasury
 
-
 __zx_plugin_name__ = "刮刮乐"
 __plugin_usage__ = f"""
 usage：
     {NICKNAME}是庄家
     指令：
-    刮刮乐 ?[数量]
+    @{NICKNAME} 刮刮乐 ?[数量]
+    {NICKNAME}刮刮乐 ?[数量]
     管理员命令：
         刮刮乐倍率 num>0.1
         刮刮乐购买量 num>=1
@@ -60,7 +63,9 @@ __plugin_configs__ = {
     }
 }
 
-ggl = on_command("刮刮乐", priority=5, block=True)
+ggl = on_command("刮刮乐", priority=5, block=True, rule=to_me())
+
+
 @ggl.handle()
 async def _(bot: Bot, event: GroupMessageEvent, arg: Message = CommandArg()):
     msg = arg.extract_plain_text().strip()
@@ -77,11 +82,13 @@ async def _(bot: Bot, event: GroupMessageEvent, arg: Message = CommandArg()):
     cost = 100 * Magnification * num
     xgold = await BagUser.get_gold(uid, gid)
     if xgold < cost:
-        await ggl.finish(f"\n金币不够还想买刮刮乐？\n您的金币余额为{str(await BagUser.get_gold(event.user_id, event.group_id))} \n刮刮乐cd: 10秒", at_sender=True)
+        await ggl.finish(
+            f"\n金币不够还想买刮刮乐？\n您的金币余额为{str(await BagUser.get_gold(event.user_id, event.group_id))} \n刮刮乐cd: 10秒",
+            at_sender=True)
 
-    #allLotteryGold = await TZlottery.getLotteryGold(gid) + int(cost*0.7)
-    allLotteryGold = await TZtreasury.get(gid) + int(cost*0.7)
-    await TZtreasury.add(group_id=gid, num=int(cost*0.2))
+    # allLotteryGold = await TZlottery.getLotteryGold(gid) + int(cost*0.7)
+    allLotteryGold = await TZtreasury.get(gid) + int(cost * 0.7)
+    await TZtreasury.add(group_id=gid, num=int(cost * 0.2))
 
     await BagUser.spend_gold(uid, gid, cost)
     text = f"花费{cost}金币购买{num}张刮刮乐\n刮开刮刮乐\n"
@@ -94,30 +101,29 @@ async def _(bot: Bot, event: GroupMessageEvent, arg: Message = CommandArg()):
         sa.append(0)
         for i in rl:
             if i > 0:
-                sa[index] += Jackpot[i-1] * Magnification
-        #sa[index] = sa[index] * 3
+                sa[index] += Jackpot[i - 1] * Magnification
+        # sa[index] = sa[index] * 3
         # 判读每档是否够用
         sa[index] = 1 if sa[index] * 3 * (30 / num) < allLotteryGold - 200 else 0
 
     # 如果连0.1的期望都不够 那就不消耗金库
     if max(sa) == 0:
         spend = False
-        
 
     # 档位
     level = sa.index(1) if max(sa) == 1 else len(Jackpot_Values) - 1
     if setJackpotLevel != -1:
         level = setJackpotLevel
     elif level != len(Jackpot_Values) - 1:
-        level = random.randint(level,len(Jackpot_Values) - 1)
+        level = random.randint(level, len(Jackpot_Values) - 1)
 
-    #调试用
+    # 调试用
     text += f"当前档位：{level}\n"
 
     allGet = 0
     b = {}
     for i in rl:
-        b[i] = (b[i]+1) if i in b else 1
+        b[i] = (b[i] + 1) if i in b else 1
 
     # 防止 特等奖 超过2次
     if 0 in b and b[0] > 2:
@@ -133,26 +139,29 @@ async def _(bot: Bot, event: GroupMessageEvent, arg: Message = CommandArg()):
         text += f"{Plevel[i]}:{b[i]}次  ("
         if i == 0:
             allGet += int(20 * b[i] * allLotteryGold / 100)
-            text += f"Wow你拿走了 福利金库中的({20*b[i]})%({ int( 20 * b[i] * allLotteryGold / 100)})\n"
+            text += f"Wow你拿走了 福利金库中的({20 * b[i]})%({int(20 * b[i] * allLotteryGold / 100)})\n"
         else:
-            allGet += int(Jackpot_Values[level][i-1] * Magnification * b[i])
-            text += f"{int(Jackpot_Values[level][i-1] * Magnification * b[i])})\n"
+            allGet += int(Jackpot_Values[level][i - 1] * Magnification * b[i])
+            text += f"{int(Jackpot_Values[level][i - 1] * Magnification * b[i])})\n"
 
-    text += f"\n共得到{allGet}金币\n纳税{int(allGet*0.1)}金币(10%)"
+    text += f"\n共得到{allGet}金币\n纳税{int(allGet * 0.1)}金币(10%)"
 
     if spend:
-        #await TZlottery.setLotteryGold(gid, allLotteryGold - allGet)
-        await TZtreasury.set(gid,allLotteryGold - allGet)
-    if int(allGet*0.1) != 0:
-        await TZtreasury.add(group_id=gid, num=int(allGet*0.1))
-    if int(allGet*0.9) != 0:
-        await BagUser.add_gold(uid, gid, allGet*0.9)
+        # await TZlottery.setLotteryGold(gid, allLotteryGold - allGet)
+        await TZtreasury.set(gid, allLotteryGold - allGet)
+    if int(allGet * 0.1) != 0:
+        await TZtreasury.add(group_id=gid, num=int(allGet * 0.1))
+    if int(allGet * 0.9) != 0:
+        await BagUser.add_gold(uid, gid, allGet * 0.9)
 
     await ggl.finish(image(b64=(await text2image(text, color="#f9f6f2", padding=10)).pic2bs4()))
+
 
 # 倍率调整
 Magnification = on_command(
     "刮刮乐倍率", priority=5, permission=SUPERUSER, block=True)
+
+
 @Magnification.handle()
 async def _(arg: Message = CommandArg()):
     msg = arg.extract_plain_text().strip()
@@ -161,6 +170,7 @@ async def _(arg: Message = CommandArg()):
         await Magnification.finish(f"倍率成功设置为{float(msg)}倍", at_sender=True)
     else:
         await Magnification.finish(f"参数只能为数字且不为空且大于0.1", at_sender=True)
+
 
 # 单次最大购买量调整
 maxNum = on_command("刮刮乐购买量", priority=5, permission=SUPERUSER, block=True)
@@ -174,6 +184,7 @@ async def _(arg: Message = CommandArg()):
         await maxNum.finish(f"最大购买量成功设置为{int(msg)}张", at_sender=True)
     else:
         await maxNum.finish(f"参数只能为数字且不为空", at_sender=True)
+
 
 # 奖池调整
 level = on_command("刮刮乐奖池选择", priority=5, permission=SUPERUSER, block=True)
@@ -190,6 +201,7 @@ async def _(arg: Message = CommandArg()):
             await level.finish(f"你的输入有问题\n最小为-1\最大为{len(Jackpot_Values) - 1}", at_sender=True)
     else:
         await level.finish(f"参数只能为数字且不为空", at_sender=True)
+
 
 # 奖池显示
 levelShow = on_command("刮刮乐奖池查看", priority=5, permission=SUPERUSER, block=True)
@@ -227,9 +239,9 @@ def p_random(s, p, size: int = 1):
     for b in range(size):
         sup_list = [len(str(i).split(".")[-1]) for i in p]
         top = 10 ** max(sup_list)
-        new_rate = [int(i*top) for i in p]
+        new_rate = [int(i * top) for i in p]
         rate_arr = []
-        for i in range(1, len(new_rate)+1):
+        for i in range(1, len(new_rate) + 1):
             rate_arr.append(sum(new_rate[:i]))
         rand = random.randint(1, top)
         data = None
@@ -250,7 +262,7 @@ p = [
     0.0178442694912956, 0.0356885389825911,
     0.0713770779651823, 0.0999279091512552,
     0.128478740337328, 0.256957480674656,
-    0.385436221011984+4.440892098500626e-16
+    0.385436221011984 + 4.440892098500626e-16
 ]
 # 奖池
 Jackpots = {
@@ -268,6 +280,7 @@ Jackpots = {
     0.1: [115, 95, 45, 30, 15, 10, 5, 0]
 }
 Jackpot_Values = list(Jackpots.values())
+
 
 # 数据库
 
@@ -294,7 +307,7 @@ class TZlottery(db.Model):
             return 1000
 
     @classmethod
-    async def setLotteryGold(cls,  group_id: int, num: int):
+    async def setLotteryGold(cls, group_id: int, num: int):
         query = cls.query.where(cls.group_id == group_id)
         query = query.with_for_update()
         my = await query.gino.first()
